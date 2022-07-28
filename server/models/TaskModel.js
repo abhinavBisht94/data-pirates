2d69326d26b953fa645cfa3df0b700874377a31c
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-07-28 11:15:45 +0530

    Update TaskModel.js

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema({
   title: { type: String, require: true },
-  description: { type: String, require: true },
   status: { type: Boolean, default: false },
   userId: { type: String, require: true },
 });
