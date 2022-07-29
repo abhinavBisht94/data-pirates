9a1d2cd56a4aad9ff4c7fc88decc7e1830dd8731
ShivaniS15 <shivaniekta15@gmail.com>
2022-07-29 14:12:43 +0530

    join file

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import "./CSS/App.css";
 import Navbar from "./components/Navbar";
 // import Sound from './components/Sound';
 import AllRoutes from "./pages/AllRoutes";
+import Chat from "./Chat/Chat";
+import Join from "./Chat/Join";
 
 function App() {
   return (
     <div>
       <AllRoutes />
+      <Chat />
     </div>
   );
 }
---
+++ client/src/Chat/Join.js
@@ -0,0 +1,39 @@
+import React from "react";
+import io from "socket.io-client";
+import { useState } from 'react';
+import Chat from './Chat';
+const socket = io.connect("http://localhost:3001");
+
+function Join() {
+    const [username,setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
+
+    const joinRoom=()=>{
+        if(username !== "" && room !== ""){
+        socket.emit("join_room", room);
+        setShowChat(true);
+        }
+    }
+    return (
+    <div className="">
+        {!showChat ?(
+        <div>
+        <h3>Chat Room</h3>
+        <input type="text" placeholder="Name" onChange={(event)=>{
+        setUsername(event.target.value);
+        }} />
+        <input type="text" placeholder="Room ID"  onChange={(event)=>{
+        setRoom(event.target.value);
+        }} />
+        <button onClick={joinRoom}>Enter Room</button>
+        </div>
+        )
+        :(
+        <Chat socket={socket}  username={username} room={room} />
+        )};
+    </div>
+    );
+}
+
+export default Join;
